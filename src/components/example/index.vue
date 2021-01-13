58f1d52bc7a5b254aa268c003d8fe1abf10fc458
Bener <2388697970@qq.com>
2021-01-13 23:01:33 +0800

    fix: npm files option

---
+++ src/components/example/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <button @click="click">{{ value }}</button>
+</template>
+
+<script>
+export default {
+  props: {
+    // v-model
+    value: {
+      type: Number,
+      default: 0
+    }
+  },
+  methods: {
+    click() {
+      this.$emit('input', this.value + 1)
+      this.$emit('change', this.value + 1)
+    }
+  }
+}
+</script>
