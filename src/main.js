1ec5b88df1f49019cec3ef97233115de7f1eb3fe
fanbj <fanbj@getui.com>
2021-03-01 13:11:39 +0800

    fix: main entry file

--- src/main.js
+++ src/main.js
@@ -5,7 +5,23 @@ import GtExample from '@components/gt-example'
 import GtInput from '@components/gt-input'
 import GtTable from '@components/gt-table'
 
+export { DatePicker, GtTablePagination, GtExample, GtInput, GtTable }
+
+function install(Vue) {
+  ;[
+    'DatePicker',
+    'GtTablePagination',
+    'GtExample',
+    'GtInput',
+    'GtTable',
+  ].forEach(lib => {
+    Vue.use(lib, lib)
+  })
+}
+
 export default {
+  version: '0.1.5',
+  install,
   DatePicker,
   GtTablePagination,
   GtExample,
